102068ba32b0b75f6b748e6e0e0162af8c58bc41
AndrewOrl <shiniasse.andyorl@gmail.com>
2025-02-12 23:36:33 +0000

    [UPDATE]: css file is always available now

--- src/llm/scripts/code/indexPage.js
+++ src/llm/scripts/code/indexPage.js
@@ -9,7 +9,7 @@ module.exports = function getIndexPageScript(description, dependencies, structur
         2. If you have any heavy framework, leave the content part to be filled by them
         3. If you have no any specific framework, you should at least have semantic & good HTML structure
         4. DO NOT GENERATE ANYTHING BUT HTML CODE.
-        5. YOU ALWAYS HAVE ACCESS ONLY TO \`style.css\` STYLES FILE. Do NOT add any other of them in html code.
+        5. YOU ALWAYS HAVE ACCESS ONLY TO \`styles.css\` STYLES FILE. Do NOT add any other of them in html code.
         5. Try to analyze script tags you lay in HTML. Maybe you don't need them. Think for 5 more seconds about it.
         6. Don't be hasty, take your time.
 
