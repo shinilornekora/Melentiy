33c84a3f79ab07626761d4baa2b9b8cc2dbedb37
AndrewOrl <shiniasse.andyorl@gmail.com>
2025-05-09 02:53:21 +0100

    remove dist folder from git

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HOSTNAME = exports.PORT = void 0;
-exports.PORT = 3001;
-exports.HOSTNAME = 'http://localhost';
